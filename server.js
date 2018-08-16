c19349b8d553cd3aba23f1b0013c14b05d4f01b5
umairriaz90 <umairriaz90@gmail.com>
2018-08-16 11:38:18 +0500

    added msg with picture support and edit delete buttons

--- server.js
+++ server.js
@@ -1,16 +1,44 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var multer = require('multer');
+var bodyParser = require('body-parser');
 users = [];
 connections = [];
+filename = '';
+app.use(bodyParser.json());
+var Storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, "./public/Images");
+    },
+    filename: function (req, file, callback) {
+        filename = file.fieldname + "_" + Date.now() + "_" + file.originalname;
+        callback(null, file.fieldname + "_" + Date.now() + "_" + file.originalname);
+    }
+});
+
+app.use('/public',express.static(__dirname + '/public'));
+
+var upload = multer({ storage: Storage }).array("imgUploader", 3); //Field name and max count
 
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/client.html');
+    res.sendFile(__dirname + '/client.html');
+});
+
+app.post("/api/upload", function (req, res) {
+    upload(req, res, function (err) {
+        if (err) {
+            return res.end("Something went wrong!");
+        }
+        return res.end("success !");
+    });
 });
 
 io.on('connection', function(socket) {
   connections.push(socket);
+
   console.log('Connected: %s sockets connected', connections.length);
 
   //Disconnect
@@ -23,8 +51,7 @@ io.on('connection', function(socket) {
 
   // Send Message
   socket.on('send message',function(data) {
-    console.log(data);
-    io.emit('new message', {msg : data, user: socket.username});
+    io.emit('new message', {msg : data, user: socket.username, filename:filename });
   });
 
   // New User
@@ -43,7 +70,7 @@ io.on('connection', function(socket) {
   }
   
 });
-
+    
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
